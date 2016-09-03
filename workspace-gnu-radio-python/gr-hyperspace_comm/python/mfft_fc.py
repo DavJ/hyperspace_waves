bdb926e91f669c3e4853e7a8b081fd78524c608e
David Jaros <jdavid.cz@gmail.com>
2016-09-03 03:25:57 +0200

    added docstring for mfft_fc block

--- workspace-gnu-radio-python/gr-hyperspace_comm/python/mfft_fc.py
+++ workspace-gnu-radio-python/gr-hyperspace_comm/python/mfft_fc.py
@@ -26,27 +26,41 @@ from hyperspace_comm.transform.transform import mfft
 class mfft_fc(gr.basic_block):
     """
     docstring for block mfft_fc
+
+    this block performs modified transform
+
+    :Arguments
+
+    p .... dumping parameter for mmft (use values +1/-1 for invariant pulses analysis, 0 for regular (slower) FFT) 
+    N .... number of input samples required for analysis 
+    M .... scrolling parameter tells, by how many samples we shift after analysis
+
+    :Returns
+ 
+    list of complex values of length N, representing the results of the transform 
+    
     """
-    def __init__(self, p, N):
+    def __init__(self, p, N, M):
         gr.basic_block.__init__(self,
             name="mfft_fc",
             in_sig=[numpy32.float],
             out_sig=[numpy.complex32])
         self.p = p
         self.N = N
+        self.M = M
 
     def forecast(self, noutput_items, ninput_items_required):
         #setup size of input_items[i] for work call
         for i in range(len(ninput_items_required)):
             #ninput_items_required[i] = noutput_items
-	    ninput_items_required[i] = N
+	    ninput_items_required[i] = self.N
              
 
     def general_work(self, input_items, output_items):
         output_items[0][:] = mfft(input_items[0], self.p)
         #consume(0, len(input_items[0]))
         #self.consume_each(len(input_items[0]))
-        self.consume_each(1)
+        self.consume_each(self.M)
 
         #return len(output_items[0])
         return self.N
