d827b7f82e25ea32a6371c6377c2e6f90a7b4fa2
David Jaros <jdavid.cz@gmail.com>
2018-01-10 22:22:45 +0100

    implemented more tests

---
+++ workspace-gnu-radio-python/gr-hyperspace_comm/python/qa_mfft_fc.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2016 <+DAVID JAROS @ OCTONION-MULTIVERSE.COM+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+from gnuradio import gr, gr_unittest
+from gnuradio import blocks
+from mfft_fc import mfft_fc
+
+from scipy.fftpack import fft
+
+class qa_mfft_fc (gr_unittest.TestCase):
+
+    def setUp (self):
+        self.tb = gr.top_block ()
+
+    def tearDown (self):
+        self.tb = None
+
+    def test_001_mfft_0 (self):
+	"""
+        this test tests, that for p=0, block returns same results as regular fft block 
+
+        """
+        # set up fg
+        #self.tb.run ()
+        # check data
+        src_data = (0, 1, 2, 3, 4, 5, 6, 7)
+        
+        expected_source_data=[list(src_data)[i:i+2] for i in range(len(src_data)-1)]
+
+
+        print "Expected source data" + str(expected_source_data)
+ 
+        expected_results_nested=[list(fft(a)) for a in expected_source_data]
+        expected_results=[val for sublist in expected_results_nested for val in sublist]
+         
+        print "Expected data" + str(expected_results)
+
+        #source - float _f  
+        src = blocks.vector_source_f(src_data)
+
+        #regular fft p=0, number of items to analyze N=2, slice 1 sample after analysis M=1
+	mfft = mfft_fc(0, 2, 1)
+
+        #destination - complex sink _c!
+	dst = blocks.vector_sink_c(1)
+
+        #connect everything together
+	self.tb.connect(src, mfft)
+	self.tb.connect(mfft, dst)
+
+        #run 
+        self.tb.run()
+	result_data = dst.data()
+        print "Result data: " + str(result_data)
+
+        #assert
+	self.assertComplexTuplesAlmostEqual(expected_results, result_data, 2)
+
+    def test_002_mfft_0 (self):
+        """
+        this test tests, that for p=0, block returns same results as regular fft block
+
+        """
+        # set up fg
+        #self.tb.run ()
+        # check data
+        src_data = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+        expected_source_data=[list(src_data)[i:i+4] for i in range(len(src_data)-3)]
+
+        print "Expected source data" + str(expected_source_data)
+
+        expected_results_nested=[list(fft(a)) for a in expected_source_data]
+        expected_results=[val for sublist in expected_results_nested for val in sublist]
+
+        print "Expected data" + str(expected_results)
+
+        #source - float _f
+        src = blocks.vector_source_f(src_data)
+
+        #regular fft p=0, number of items to analyze N=2, slice 1 sample after analysis M=1
+        mfft = mfft_fc(0, 4, 1)
+
+        #destination - complex sink _c!
+        dst = blocks.vector_sink_c(1)
+
+        #connect everything together
+        self.tb.connect(src, mfft)
+        self.tb.connect(mfft, dst)
+
+        #run
+        self.tb.run()
+        result_data = dst.data()
+        print "Result data: " + str(result_data)
+
+        #assert
+        self.assertComplexTuplesAlmostEqual(expected_results, result_data, 2)
+
+    def test_003_mfft_0 (self):
+        """
+        this test tests, that for p=0, block returns same results as regular fft block
+
+        """
+        # set up fg
+        #self.tb.run ()
+        # check data
+        src_data = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+        expected_source_data=[list(src_data)[i:i+8] for i in range(len(src_data)-7)]
+
+        print "Expected source data" + str(expected_source_data)
+
+        expected_results_nested=[list(fft(a)) for a in expected_source_data]
+        expected_results=[val for sublist in expected_results_nested for val in sublist]
+
+        print "Expected data" + str(expected_results)
+
+        #source - float _f
+        src = blocks.vector_source_f(src_data)
+
+        #regular fft p=0, number of items to analyze N=2, slice 1 sample after analysis M=1
+        mfft = mfft_fc(0, 8, 1)
+
+        #destination - complex sink _c!
+        dst = blocks.vector_sink_c(1)
+
+        #connect everything together
+        self.tb.connect(src, mfft)
+        self.tb.connect(mfft, dst)
+
+        #run
+        self.tb.run()
+        result_data = dst.data()
+        print "Result data: " + str(result_data)
+
+        #assert
+        self.assertComplexTuplesAlmostEqual(expected_results, result_data, 2)
+
+if __name__ == '__main__':
+    gr_unittest.run(qa_mfft_fc, "qa_mfft_fc.xml")
